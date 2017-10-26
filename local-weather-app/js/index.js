fcd6ac23aa53e73e758cca4874e7f7b07a54c82c
Venessa Yang <v-vyang@v-vyang-mac.tsi.lan>
2017-10-26 10:53:32 -0700

    js  changes

--- local-weather-app/js/index.js
+++ local-weather-app/js/index.js
@@ -11,54 +11,39 @@ function getLocation() {
 
 function getWeather(lat, lon, countryCode) {
   var weatherAPI = 'https://fcc-weather-api.glitch.me/api/current?lat=' +
-    lat + '&lon=' + lon + '&units=imperial' + '&type=accurate' + callback;
-  $.getJSON(weatherAPI, function(weatherData) {
-      tempC = weatherData.main.temp.toFixed(0);
-      tempF = (tempC * (9 / 5)+32).toFixed(0);
-
-      var condition = weatherData.weather[0].description,
-        id = weatherData.weather[0].id,
-        deg = weatherData.wind.deg,
-        iconClass,
-        bgIndex,
-        backgroundId = [ 299, 599, 699],
-        backgroundIcon = [
-          'Thunderstorm',
-          'Rain',
-          'Snow',
-          'Clouds,Mist',
-        ],
-        backgroundImg = [
-          'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/Thunderstorm-5best.jpg?raw=true',
-          'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/rain2.jpg?raw=true',
-          'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/Snow.jpg?raw=true',
-          'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/cloudy-field-2.jpg?raw=true'
-        ];
-
-      backgroundId.push(id);
-      bgIndex = backgroundId.sort().indexOf(id);
-      $('body').css('background-image', 'url(' + backgroundImg[bgIndex] + ')');
-      iconClass = backgroundIcon[bgIndex];
-
-      // for(var i = 0; i<backgroundIcon.length; i++){
-      //   console.log(weatherData.weather[0].main);  
-      //   if(backgroundIcon[i].indexOf(weatherData.weather[0].main)>-1){
-      //     $('body').css('background-image', 'url(' + backgroundImg[i] + ')');
-      //     iconClass = backgroundIcon[i];
-      //   } 
-      // }
-      
-      
-      $('#temperature').text(tempF + '° F');
-      $('#condition').html(
-        '<i class="wi wi-' + iconClass + '"></i><br>' + condition);
-    })
+    lat + '&lon=' + lon + '&units=imperial' + '&type=accurate' ;
+  $.getJSON(weatherAPI, success)
     .fail(function(err) {
-      alert('There was an error retrieving your weather data. \n' +
-        'Please try again later.');
+      alert('Error');
     });
 }
+var success = function(weatherData) {
+  var tempC = weatherData.main.temp.toFixed(0),
+      tempF = (tempC * (9 / 5)+32).toFixed(0),
+      condition = weatherData.weather[0].description,
+      id = weatherData.weather[0].id,
+      backgroundIds = [ 299, 599, 699],
+      weathers = [
+        'thunderstorm',
+        'rain',
+        'snow',
+        'cloudy',
+      ],
+      backgroundImg = [
+        'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/Thunderstorm-5best.jpg?raw=true',
+        'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/rain2.jpg?raw=true',
+        'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/Snow.jpg?raw=true',
+        'https://github.com/xy7313/FrontDemo/blob/master/local-weather-app/imgs/cloudy-field-2.jpg?raw=true'
+      ];
 
+  backgroundIds.push(id);
+  var bgIndex = backgroundIds.sort().indexOf(id);
+  var weatherIcon = weathers[bgIndex];      
+  $('body').css('background-image', 'url(' + backgroundImg[bgIndex] + ')');      
+  $('#temperature').text(tempF + '° F');
+  $('#condition').html(
+    '<i class="wi wi-' + weatherIcon + '"></i><br>' + condition);
+}
 //toggle between c/f
 $('#convert-button').click(function() {
   if ($('#temperature').text().indexOf('F') > -1) {
@@ -66,7 +51,6 @@ $('#convert-button').click(function() {
   } else {
     $('#temperature').text(tempF + '° F');
   }
-
   this.blur(); 
 });
 
@@ -251,4 +235,4 @@ $('#convert-button').click(function() {
 
 
 // get your own app ID at: https://home.openweathermap.org/api_keys
-var callback = '&APPID=2c542b9bff6a7688e92ddecc613993c6';
+// var callback = '&APPID=2c542b9bff6a7688e92ddecc613993c6';
